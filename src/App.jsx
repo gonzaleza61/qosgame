d7de34f26b0d8836b74d7f2f7fbc049c3f3a553c
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-06-29 12:10:18 -0500

    wtf

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
-import { useMemo } from "react";
 import "@pixi/events";
 
-import { BlurFilter, TextStyle } from "pixi.js";
+import { TextStyle } from "pixi.js";
 import { Stage, Container, Sprite, Text } from "@pixi/react";
 
 export const App = () => {
-  const blurFilter = useMemo(() => new BlurFilter(2), []);
   const bunnyUrl = "https://pixijs.io/pixi-react/img/bunny.png";
   return (
-    <Stage x={800} y={600} options={{ background: 0x1099bb }}>
+    <Stage x={1920} y={1080} options={{ background: 0x1099bb }}>
       <Sprite image={bunnyUrl} x={300} y={150} />
       <Sprite image={bunnyUrl} x={500} y={150} />
       <Sprite image={bunnyUrl} x={400} y={200} />
@@ -20,7 +18,6 @@ export const App = () => {
           anchor={0.5}
           x={220}
           y={150}
-          filters={[blurFilter]}
           style={
             new TextStyle({
               align: "center",
