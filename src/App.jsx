2635b8130980f6ef1be486f253d9fd1050f692d2
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-06-29 13:49:54 -0500

    sprite position bottom left

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
 import "./App.css";
 import "@pixi/events";
 
-import { TextStyle } from "pixi.js";
-import { Stage, Container, Sprite, Text } from "@pixi/react";
+import { useRef, useEffect } from "react";
+import { Stage, Sprite, Container } from "@pixi/react";
 
 export const App = () => {
   const bunnyUrl = "https://pixijs.io/pixi-react/img/bunny.png";
-  return (
-    <Stage x={1920} y={1080} options={{ background: 0x1099bb }}>
-      <Sprite image={bunnyUrl} x={300} y={150} />
-      <Sprite image={bunnyUrl} x={500} y={150} />
-      <Sprite image={bunnyUrl} x={400} y={200} />
+  const stageRef = useRef();
+  const bunnyRef = useRef();
 
-      <Container x={200} y={200}>
-        <Text
-          text="QoS Game"
-          anchor={0.5}
-          x={220}
-          y={150}
-          style={
-            new TextStyle({
-              align: "center",
-              fill: "0xffffff",
-              fontSize: 50,
-              letterSpacing: 20,
-              dropShadow: true,
-              dropShadowColor: "#E72264",
-              dropShadowDistance: 6,
-            })
-          }
-        />
-      </Container>
+  console.log(bunnyRef.current.height);
+
+  return (
+    <Stage
+      ref={stageRef}
+      width={1000}
+      height={500}
+      options={{ background: 0x1099bb }}
+    >
+      <Sprite ref={bunnyRef} image={bunnyUrl} position={[0, 463]} />
     </Stage>
   );
 };
+
+export default App;
